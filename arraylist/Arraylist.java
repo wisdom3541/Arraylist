3180ce1c9ce332b1140ac68f7947be250b299f17
E.Wisdom <44945885+wisdom3541@users.noreply.github.com>
2019-01-27 10:19:22 -0800

    Add files via upload

---
+++ arraylist/Arraylist.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylist;
+
+import java.util.*;
+/**
+ *
+ * @author Bossbrown
+ */
+public class Arraylist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //creating array list
+        {ArrayList<String> name = new ArrayList<String>();
+        //adding to the arraylist
+        name.add("lily");
+        name.add("wisdom");
+        name.add("ella");
+        name.add("ayus");
+        
+        //printing arraylist with iterator
+        Iterator i = name.iterator();
+        while(i.hasNext())
+        {
+                System.out.println(i.next());
+                        }
+        
+        
+    }
+    }
+}
+    
+
